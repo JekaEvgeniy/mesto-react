02fb0098a67c6a650aa7f41e6dd78674ba44cf62
JekaEvgeniy <pruven@yandex.ru>
2023-06-03 02:19:47 +0300

    import logo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import headerLogo from './images/header/header-logo.svg';
 import './index.css';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
 	<div className="page">
 		<header className="header">
-			<img className="header__logo" src="./images/header/header-logo.svg" alt="Логотип" loading="lazy" />
+				<img className="header__logo" src={headerLogo} alt="Логотип" loading="lazy" />
 		</header>
 		<main className="content">
 			<section className="profile">
