12f36e7ef6ab23f7f86b89e2809318c19aa37642
JekaEvgeniy <pruven@yandex.ru>
2023-06-04 13:07:40 +0300

    feat: api user info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,39 @@
+import React from "react";
+import api from '../utils/Api';
+
 function Main(props){
+
+	const [userName, setUserName] = React.useState('');
+	const [userDescription, setUserDescription] = React.useState('');
+	const [userAvatar, setUserAvatar] = React.useState('#');
+
+	React.useEffect( () => {
+		api.getUserInfo()
+			.then( res => {
+				// console.log(res);
+
+				setUserName(res.name);
+				setUserDescription(res.about);
+				setUserAvatar(res.avatar);
+			})
+			.catch( err => console.error(err) );
+	})
+
 	return (
 		<main className="content">
 			<section className="profile">
 				<div className="profile__figure">
-					<img className="profile__avatar" src="#" alt="Фотография" />
+					<img className="profile__avatar" src={userAvatar} alt="Фотография" />
 					<button onClick={props.onEditAvatar} className="profile__button profile__button_type_avatar" type="button" name="button"
 						aria-label="Обновить фотографию"></button>
 				</div>
 
 				<div className="profile__description">
 					<div className="profile__info">
-						<h1 className="profile__header"></h1>
+						<h1 className="profile__header">{userName}</h1>
 						<button onClick={props.onEditProfile} className="profile__button profile__button_type_edit" type="button" name="button" aria-label="Редактировать"></button>
 					</div>
-					<p className="profile__subtitle"></p>
+					<p className="profile__subtitle">{userDescription}</p>
 				</div>
 
 				<button onClick={props.onAddPlace} className="profile__button profile__button_type_add" type="button" name="button" aria-label="Добавить"></button>
