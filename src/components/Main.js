c03badcaec74fe423ff2026c8fe81c97e38db5f0
JekaEvgeniy <pruven@yandex.ru>
2023-06-12 00:02:23 +0300

    feat: context Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,25 +2,29 @@ import React from "react";
 import api from '../utils/Api';
 import Card from '../components/Card';
 
+import CurrentUserContext from '../contexts/CurrentUserContext';
+
 function Main(props){
 
-	const [userName, setUserName] = React.useState('');
-	const [userDescription, setUserDescription] = React.useState('');
-	const [userAvatar, setUserAvatar] = React.useState('#');
-
 	const [cards, setCards] = React.useState([]);
+	const currentUser = React.useContext(CurrentUserContext);
+	console.log('currentUser =', currentUser);
 
-	React.useEffect( () => {
-		api.getUserInfo()
-			.then( res => {
+	// const [userName, setUserName] = React.useState('');
+	// const [userDescription, setUserDescription] = React.useState('');
+	// const [userAvatar, setUserAvatar] = React.useState('#');
 
-				setUserName(res.name);
-				setUserDescription(res.about);
-				setUserAvatar(res.avatar);
+	// React.useEffect( () => {
+	// 	api.getUserInfo()
+	// 		.then( res => {
 
-			})
-			.catch( err => console.error(err) );
-	}, [])
+	// 			setUserName(res.name);
+	// 			setUserDescription(res.about);
+	// 			setUserAvatar(res.avatar);
+
+	// 		})
+	// 		.catch( err => console.error(err) );
+	// }, [])
 
 	React.useEffect( () => {
 		api.getCards()
@@ -32,22 +36,27 @@ function Main(props){
 			.catch( err => console.error(err) );
 	}, [])
 
+	if ( !currentUser ) {
+		// При обновлении данные не успевают загружаться и вываливаются ошибки оО
+		return;
+	}
+
 	return (
 
 		<main className="content">
 			<section className="profile">
 				<div className="profile__figure">
-					<img className="profile__avatar" src={userAvatar} alt="Фотография" />
+					<img className="profile__avatar" src={currentUser.avatar} alt="Фотография" />
 					<button onClick={props.onEditAvatar} className="profile__button profile__button_type_avatar" type="button" name="button"
 						aria-label="Обновить фотографию"></button>
 				</div>
 
 				<div className="profile__description">
 					<div className="profile__info">
-						<h1 className="profile__header">{userName}</h1>
+						<h1 className="profile__header">{currentUser.name}</h1>
 						<button onClick={props.onEditProfile} className="profile__button profile__button_type_edit" type="button" name="button" aria-label="Редактировать"></button>
 					</div>
-					<p className="profile__subtitle">{userDescription}</p>
+					<p className="profile__subtitle">{currentUser.about}</p>
 				</div>
 
 				<button onClick={props.onAddPlace} className="profile__button profile__button_type_add" type="button" name="button" aria-label="Добавить"></button>
